81d0b9267214704b240c56d2f0827acfbb592b3a
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2014-11-12 17:05:00 +0900

    Add `updateAsakusafw` task to organizer (#23).

--- gradle-sdk-project/asakusa-gradle-plugins/src/main/groovy/com/asakusafw/gradle/plugins/AsakusafwOrganizerPlugin.groovy
+++ gradle-sdk-project/asakusa-gradle-plugins/src/main/groovy/com/asakusafw/gradle/plugins/AsakusafwOrganizerPlugin.groovy
@@ -304,28 +304,69 @@ class AsakusafwOrganizerPlugin  implements Plugin<Project> {
     private void configureDevelopmentProfileTasks() {
         AsakusafwOrganizer organizer = organizers[PROFILE_NAME_DEVELOPMENT]
         organizer.task('gatherAsakusafw').dependsOn organizer.task('cleanAssembleAsakusafw')
-        project.tasks.create('installAsakusafw') {
-            dependsOn organizer.task('gatherAsakusafw')
-            group ASAKUSAFW_ORGANIZER_GROUP
-            description "Installs Asakusa Framework to \$ASAKUSA_HOME using '${organizer.profile.name}' profile."
+        project.tasks.create('backupAsakusafw') {
+            shouldRunAfter organizer.task('gatherAsakusafw')
+            onlyIf { getFrameworkHome() != null }
             doLast {
-                if (!System.env['ASAKUSA_HOME']) {
-                    throw new RuntimeException('ASAKUSA_HOME is not defined')
-                }
-                def timestamp = new Date().format('yyyyMMddHHmmss')
+                File home = getFrameworkHome()
+                String timestamp = new Date().format('yyyyMMddHHmmss')
+                File backup = new File(home.parentFile, "${home.name}_${timestamp}")
                 project.copy {
-                    from "${System.env.ASAKUSA_HOME}"
-                    into "${System.env.ASAKUSA_HOME}_${timestamp}"
+                    from home
+                    into backup
                 }
-                project.delete "${System.env.ASAKUSA_HOME}"
-                project.mkdir "${System.env.ASAKUSA_HOME}"
-                project.copy {
-                    from organizer.task('gatherAsakusafw').destination
-                    into "${System.env.ASAKUSA_HOME}"
-                }
-                logger.lifecycle "Asakusa Framework has been installed on ASAKUSA_HOME: ${System.env.ASAKUSA_HOME}"
             }
         }
+        project.tasks.create('updateAsakusafw') { Task t ->
+            group ASAKUSAFW_ORGANIZER_GROUP
+            description "Updates Asakusa Framework on \$ASAKUSA_HOME using '${organizer.profile.name}' profile."
+            t.dependsOn organizer.task('gatherAsakusafw')
+            t.shouldRunAfter 'backupAsakusafw'
+            project.afterEvaluate {
+                File home = getFrameworkHome()
+                if (home != null) {
+                    t.inputs.dir organizer.task('gatherAsakusafw').destination
+                    t.outputs.dir home
+                }
+            }
+            t.doLast {
+                File home = getFrameworkHomeChecked()
+                if (home.exists()) {
+                    project.delete home
+                }
+                project.mkdir home
+                project.copy {
+                    from organizer.task('gatherAsakusafw').destination
+                    into home
+                }
+            }
+        }
+        project.tasks.create('installAsakusafw') {
+            group ASAKUSAFW_ORGANIZER_GROUP
+            description "Installs Asakusa Framework to \$ASAKUSA_HOME using '${organizer.profile.name}' profile."
+            dependsOn 'backupAsakusafw', 'updateAsakusafw'
+            doLast {
+                File home = getFrameworkHomeChecked()
+                if (project.tasks.updateAsakusafw.didWork) {
+                    logger.lifecycle "Asakusa Framework is successfully installed: ${home}"
+                } else {
+                    logger.lifecycle "Asakusa Framework is already up-to-date: ${home}"
+                }
+            }
+        }
+    }
+
+    private File getFrameworkHome() {
+        def home = System.env['ASAKUSA_HOME']
+        return home == null ? null : project.file(home).absoluteFile
+    }
+
+    private File getFrameworkHomeChecked() {
+        File home = getFrameworkHome()
+        if (home == null) {
+            throw new RuntimeException('ASAKUSA_HOME is not defined')
+        }
+        return home
     }
 
     private void configureProductionProfileTasks() {
