5a390b0c88c86af934d9e1ded51998c736d9cafa
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2016-01-21 15:05:57 +0900

    Bump up JSch version

--- gradle-sdk-project/asakusa-gradle-plugins/src/main/groovy/com/asakusafw/gradle/plugins/internal/AsakusafwInternalPluginConvention.groovy
+++ gradle-sdk-project/asakusa-gradle-plugins/src/main/groovy/com/asakusafw/gradle/plugins/internal/AsakusafwInternalPluginConvention.groovy
@@ -49,7 +49,7 @@ class AsakusafwInternalPluginConvention {
             slf4jVersion = '1.7.5'
             logbackVersion = '1.1.2'
             log4jVersion = '1.2.15'
-            jschVersion = '0.1.51'
+            jschVersion = '0.1.53'
             gsonVersion = '2.2.4'
             httpClientVersion = '4.2.5'
             commonsCliVersion = '1.2'
